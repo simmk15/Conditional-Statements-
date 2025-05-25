bd687d9c15d0c6c10119f3287c963a92390b57d9
simmk15 <sknagra15@gmail.com>
2025-05-25 12:30:48 +0530

    odd_even

---
+++ greater_smaller.py
@@ -0,0 +1,5 @@
+num=float(input("Enter the number="))
+if num>15:
+    print(num,"is greater than 15")
+else:
+    print(num,"is less than 15")
---
+++ odd_even.py
@@ -0,0 +1,5 @@
+num=float(input("Enter your number="))
+if num%2==0:
+    print(num,"is an even number")
+else:
+    print(num,"is an odd number")
